603c24e328cd10ec4aea035026c70c30e796c3a8
Will Deakin <will.deakin@crinstitute.org.uk>
2023-07-28 20:17:52 +0100

    fix library compatibility issues

--- junction.py
+++ junction.py
@@ -1,205 +1,309 @@
 #!/usr/bin/env python3
 
 """
-The junction.py script simplifies a networks by disaggregating an edge-network 
+The junction.py script simplifies a networks by disaggregating an edge-network
 and reassembling the simplified network by combining simply-connected nodes
 """
 
-import sys
-import pandas as pd
-import numpy as np
-import datetime
 import argparse
+import datetime as dt
+import sys
 
-parser = argparse.ArgumentParser(description='Input an edge file and output the corresponding .jsonl format simplified network. By default outputs to stdout and timing information to stderr')
+import pandas as pd
 
-parser.add_argument('--tsv', dest='tsv', action='store_true',
-                    default=False, help='process tsv format file')
+START_TIME = dt.datetime.now()
 
-parser.add_argument('--dump', dest='dump', action='store_true',
-                    default=False, help='dump edge file with edge IDs')
 
-parser.add_argument('--quiet', dest='quiet', action='store_true',
-                    default=False, help='suppress timing ')
-
-parser.add_argument('inputfile', type=str, help='name of edge-file to process')
-
-parser.add_argument('outputfile', type=str, default=None, nargs='?', help='name of simplified edge-file to output, default stdout')
-
-args = parser.parse_args()
-
-def time_offset(string_str, time_output=True):
-    if not args.quiet:
+def log(string_str, quiet, time_output=True):
+    if not quiet:
         if time_output:
-            sys.stderr.write('{}\t{: > 8.3f}\n'.format(string_str, (datetime.datetime.now() - start_time).total_seconds()))
+            now = (dt.datetime.now() - START_TIME).total_seconds()
+            sys.stderr.write(f"{string_str}\t{now: > 8.3f}\n")
+
         else:
-            sys.stderr.write('{}\n'.format(string_str))
+            sys.stderr.write(f"{string_str}\n")
 
-start_time = datetime.datetime.now()
-time_offset('Loading edges', False)
 
-this_filelist = args.inputfile.split('.')
-this_filename = '.'.join(this_filelist[:-1])
-this_extension = this_filelist[-1]
+def main():
+    parser = argparse.ArgumentParser(
+        description="Input an edge file and output the corresponding .jsonl format simplified network. By default outputs to stdout and timing information to stderr"
+    )
 
-if args.tsv or this_extension.lower() == 'tsv':
-    edges = pd.read_csv(args.inputfile, delimiter='\t')
-else:
-    pd.read_json(args.inputfile, lines=True)
+    parser.add_argument(
+        "--tsv",
+        dest="tsv",
+        action="store_true",
+        default=False,
+        help="process tsv format file",
+    )
 
-edges = edges.rename(columns={'_key': 'edge', 'edgeid': 'edge', 'from': 'source', 'to': 'target'})
-edges['index'] = edges['edge']
-edges = edges.set_index('index')
+    parser.add_argument(
+        "--dump",
+        dest="dump",
+        action="store_true",
+        default=False,
+        help="dump edge file with edge IDs",
+    )
 
-if not edges.index.is_unique:
-    raise ValueError('Duplicate edge IDs\n{}'.format(edges[edges.index.duplicated(False)]))
+    parser.add_argument(
+        "--quiet",
+        dest="quiet",
+        action="store_true",
+        default=False,
+        help="suppress timing",
+    )
 
-time_offset('Loaded\t\t{:> 8} edges\t'.format(len(edges.index)))
+    parser.add_argument("inputfile", type=str, help="name of edge-file to process")
 
-time_offset('Creating segments', False)
-time_offset('Adding missing edges', False)
-parallel_edges = edges[['source','target']].reset_index()
-parallel_edges = parallel_edges.merge(parallel_edges[['source', 'target']], how='left', left_on='source', right_on='target', suffixes=('', '_A')).drop(columns='target_A').dropna().drop_duplicates()
-parallel_edges = parallel_edges.merge(parallel_edges[['source', 'target']], how='left', left_on='target', right_on='source', suffixes=('', '_B')).drop(columns='source_B').dropna().drop_duplicates()
-parallel_edges = parallel_edges.query('source == target_B or target == source_A').set_index('index')
+    parser.add_argument(
+        "outputfile",
+        type=str,
+        default=None,
+        nargs="?",
+        help="name of simplified edge-file to output, default stdout",
+    )
 
-missing_index = edges.index.difference(parallel_edges.index)
+    args = parser.parse_args()
+    log("Loading edges", args.quiet, False)
 
-time_offset('Added\t\t{:>8} edges\t'.format(len(missing_index)))
-segments = pd.concat([edges, edges.loc[missing_index].rename(columns={'source': 'target', 'target': 'source'})], sort=False).reset_index()
-segments['colour'] = pd.Series(segments.index).apply(lambda x: 'C{}'.format(str(x).zfill(5)))
-segments['segment'] = segments['colour']
+    this_filelist = args.inputfile.split(".")
+    this_filename = ".".join(this_filelist[:-1])
+    this_extension = this_filelist[-1]
 
-time_offset('Counting duplicates', False)
-duplicate_segments = segments[segments.duplicated(['source', 'target'], keep=False)].sort_values(['source', 'target'])
-duplicate_segments = duplicate_segments[['edge', 'source', 'length', 'target', 'wayid']]
+    if args.tsv or this_extension.lower() == "tsv":
+        edges = pd.read_csv(args.inputfile, delimiter="\t")
+    else:
+        edges = pd.read_json(args.inputfile, lines=True)
 
-time_offset('Dumping duplicate segments', False)
-if len(duplicate_segments) > 0:
-    pd.DataFrame(duplicate_segments).to_json(this_filename + '-duplicates.jsonl', orient='records', lines=True)
+    edges = edges.rename(
+        columns={"_key": "edge", "edgeid": "edge", "from": "source", "to": "target"}
+    )
+    edges["index"] = edges["edge"]
+    edges = edges.set_index("index")
 
-time_offset('Dumped duplicate{:> 8} segments'.format(len(duplicate_segments.index)))
+    if not edges.index.is_unique:
+        raise ValueError(
+            f"Duplicate edge IDs\n{edges[edges.index.duplicated(False)]}"
+        )
 
-time_offset('Dropping duplicate segments', False)
-segments = segments.drop_duplicates(['source', 'target'])
-time_offset('Created\t\t{:> 8} segments'.format(len(segments.index)))
+    log(f"Loaded\t\t{len(edges.index):> 8} edges\t", args.quiet)
 
-time_offset('Counting nodes', False)
-node_count = segments['source'].to_frame(name='node').reset_index().rename(columns={'index': 'edge'}).groupby('node').edge.nunique().to_frame(name='n')
-time_offset('Counted\t\t{:> 8} nodes\t'.format(len(node_count.index)))
+    log("Creating segments", args.quiet, False)
+    log("Adding missing edges", args.quiet, False)
+    parallel_edges = edges[["source", "target"]].reset_index()
+    parallel_edges = (
+        parallel_edges.merge(
+            parallel_edges[["source", "target"]],
+            how="left",
+            left_on="source",
+            right_on="target",
+            suffixes=("", "_A"),
+        )
+        .drop(columns="target_A")
+        .dropna()
+        .drop_duplicates()
+    )
+    parallel_edges = (
+        parallel_edges.merge(
+            parallel_edges[["source", "target"]],
+            how="left",
+            left_on="target",
+            right_on="source",
+            suffixes=("", "_B"),
+        )
+        .drop(columns="source_B")
+        .dropna()
+        .drop_duplicates()
+    )
+    parallel_edges = parallel_edges.query(
+        "source == target_B or target == source_A"
+    ).set_index("index")
 
-time_offset('Creating links', False)
+    missing_index = edges.index.difference(parallel_edges.index)
 
-df0 = segments[['source', 'target']]
-df0 = df0.join(node_count['n'], on='source').rename(columns={'n': 'm'})
-df0 = df0.join(node_count['n'], on='target')
+    log(f"Added\t\t{len(missing_index):>8} edges\t", args.quiet)
+    segments = pd.concat(
+        [
+            edges,
+            edges.loc[missing_index].rename(
+                columns={"source": "target", "target": "source"}
+            ),
+        ],
+        sort=False,
+    ).reset_index()
+    segments["colour"] = pd.Series(segments.index).apply(
+        lambda x: f"C{str(x).zfill(5)}"
+    )
+    segments["segment"] = segments["colour"]
 
-df1 = df0.reset_index().set_index('source').query('m < 3').rename(columns={'index': 'target_ix'}).drop(columns=['m', 'n'])
-df2 = df0.reset_index().set_index('target').rename(columns={'index': 'source_ix'})
+    log("Counting duplicates", args.quiet, False)
+    duplicate_segments = segments[
+        segments.duplicated(["source", "target"], keep=False)
+    ].sort_values(["source", "target"])
+    duplicate_segments = duplicate_segments[
+        ["edge", "source", "length", "target", "wayid"]
+    ]
 
-links = df2.join(df1).query('source != target').dropna().astype({'target_ix': int})
-links = links.reindex(links.index.rename('node')).reset_index()
-links.index = links['source_ix']
-links['count'] = 0
+    log("Dumping duplicate segments", args.quiet, False)
+    if len(duplicate_segments) > 0:
+        pd.DataFrame(duplicate_segments).to_json(
+            f"{this_filename}-duplicates.jsonl", orient="records", lines=True
+        )
 
-time_offset('Created\t\t{:> 8} links\t'.format(len(links.index)))
-time_offset('Processing segments', False)
-links['active'] = ((links['n'] == 2) & (links['m'] != 2)) 
+    log(
+        f"Dumped duplicate{len(duplicate_segments.index):> 8} segments",
+        args.quiet,
+    )
 
-count = 0
-while links['active'].any():    
-    for this_id in links.query('active').index:
+    log("Dropping duplicate segments", args.quiet, False)
+    segments = segments.drop_duplicates(["source", "target"])
+    log(f"Created\t\t{len(segments.index):> 8} segments", args.quiet)
+
+    log("Counting nodes", args.quiet, False)
+    node_count = (
+        segments["source"]
+        .to_frame(name="node")
+        .reset_index()
+        .rename(columns={"index": "edge"})
+        .groupby("node")
+        .edge.nunique()
+        .to_frame(name="n")
+    )
+    log(f"Counted\t\t{len(node_count.index):> 8} nodes\t", args.quiet)
+
+    log("Creating links", args.quiet, False)
+
+    df0 = segments[["source", "target"]]
+    df0 = df0.join(node_count["n"], on="source").rename(columns={"n": "m"})
+    df0 = df0.join(node_count["n"], on="target")
+
+    df1 = (
+        df0.reset_index()
+        .set_index("source")
+        .query("m < 3")
+        .rename(columns={"index": "target_ix"})
+        .drop(columns=["m", "n"])
+    )
+    df2 = df0.reset_index().set_index("target").rename(columns={"index": "source_ix"})
+
+    links = df2.join(df1).query("source != target").dropna().astype({"target_ix": int})
+    links = links.reindex(links.index.rename("node")).reset_index()
+    links.index = links["source_ix"]
+    links["count"] = 0
+
+    log(f"Created\t\t{len(links.index):> 8} links\t", args.quiet)
+    log("Processing segments", args.quiet, False)
+    links["active"] = (links["n"] == 2) & (links["m"] != 2)
+
+    count = 0
+    while links["active"].any():
+        for this_id in links.query("active").index:
+            if count % 1024 == 0:
+                log(
+                    "Active\t\t{len(links.query('active').index):> 8} segments",
+                    args.quiet,
+                )
+            links.at[this_id, "active"] = False
+            next_id = links.at[this_id, "target_ix"]
+            links.at[this_id, "count"] += 1
+            while True:
+                segments.at[next_id, "segment"] = segments.at[this_id, "segment"]
+                this_id = next_id
+
+                if this_id not in links.index:
+                    break
+                next_id = links.at[this_id, "target_ix"]
+                links.at[this_id, "count"] += 1
+            count += 1
+    log(
+        f"Active\t\t{len(links.query('active').index):> 8} segments", args.quiet
+    )
+
+    links["loop"] = (links["n"] == 2) & (links["m"] == 2) & (links["count"] == 0)
+    links["active"] = links["loop"]
+    log(f"Processed\t{len(segments.index):> 8} segments", args.quiet)
+
+    log("Processing loops", args.quiet, False)
+    count = 0
+    for start_id in links.query("active").index:
         if count % 1024 == 0:
-            time_offset('Active\t\t{:> 8} segments'.format(len(links.query('active').index)))
-        links.at[this_id, 'active'] = False
-        next_id = links.at[this_id, 'target_ix']
-        links.at[this_id, 'count'] += 1
-        while True:
-            segments.at[next_id, 'segment'] = segments.at[this_id, 'segment']
+            log(
+                f"Active\t\t{len(links.query('active').index):> 8} loops\t",
+                args.quiet,
+            )
+        this_id = start_id
+        for _ in range(0, 3):
+            links.at[this_id, "count"] += 1
+            links.at[this_id, "active"] = False
+            next_id = links.at[this_id, "target_ix"]
+            segments.at[next_id, "segment"] = segments.at[this_id, "segment"]
             this_id = next_id
-
-            if this_id not in links.index:
-                break
-            next_id = links.at[this_id, 'target_ix']
-            links.at[this_id, 'count'] += 1
         count += 1
-time_offset('Active\t\t{:> 8} segments'.format(len(links.query('active').index)))
 
-links['loop'] = ((links['n'] == 2) & (links['m'] == 2) & (links['count'] == 0))
-links['active'] = links['loop']
-time_offset('Processed\t{:> 8} segments'.format(len(segments.index)))
+    log(f"Active\t\t{len(links.query('active').index):> 8} loops\t", args.quiet)
+    log(f"Processed\t{len(links.query('loop').index):> 8} loops\t", args.quiet)
 
-time_offset('Processing loops', False)
-count = 0
-for start_id in links.query('active').index:
-    if count % 1024 == 0:
-        time_offset('Active\t\t{:> 8} loops\t'.format(len(links.query('active').index)))
-    this_id = start_id
-    for i in range(0, 3):
-        links.at[this_id, 'count'] += 1
-        links.at[this_id, 'active'] = False
-        next_id = links.at[this_id, 'target_ix']
-        segments.at[next_id, 'segment'] = segments.at[this_id, 'segment']
-        this_id = next_id
-    count += 1
+    if args.dump:
+        log("Dumping segments", False)
+        pd.DataFrame(segments).T.to_json(this_filename + "-dump.jsonl")
+        log(f"Dumped\t\t{len(segments.index):> 8} segments", args.quiet)
 
-time_offset('Active\t\t{:> 8} loops\t'.format(len(links.query('active').index)))
-time_offset('Processed\t{:> 8} loops\t'.format(len(links.query('loop').index)))
+    log("Outputing segments", args.quiet, False)
 
-if args.dump:
-    time_offset('Dumping segments', False)
-    pd.DataFrame(segments).T.to_json(this_filename + '-dump.jsonl')
-    time_offset('Dumped\t\t{:> 8} segments'.format(len(segments.index)))
+    output_segments = segments.query("colour == segment")[
+        ["source", "target", "segment"]
+    ]
+    output_segments["nodes"] = output_segments.apply(lambda x: [], axis=1)
+    output_segments["wayids"] = output_segments.apply(lambda x: [], axis=1)
+    output_segments["length"] = -0.0
 
-time_offset('Outputing segments', False)
+    count = 0
+    for start_id in segments.query("colour == segment").index:
+        if count % 8192 == 0:
+            log(f"Output\t\t{count:> 8} segments", args.quiet)
 
-output_segments = segments.query('colour == segment')[['source', 'target', 'segment']]
-output_segments['nodes'] = output_segments.apply(lambda x: list(), axis=1)
-output_segments['wayids'] = output_segments.apply(lambda x: list(), axis=1)
-output_segments['length'] = -0.0
+        start_node = segments.at[start_id, "source"]
+        end_node = segments.at[start_id, "target"]
+        these_nodes = [start_node, end_node]
+        length = segments.at[start_id, "length"]
+        these_edges = [segments.at[start_id, "edge"]]
+        this_wayid = segments.at[start_id, "wayid"]
+        wayids = [this_wayid]
+        this_id = start_id
+        while this_id in links.index:
+            this_id = links.at[this_id, "target_ix"]
+            if (
+                links.at[start_id, "loop"]
+                and start_id == links.at[this_id, "target_ix"]
+            ):
+                break
+            this_node = segments.at[this_id, "target"]
+            if this_node == start_node:
+                break
+            these_nodes.append(this_node)
+            these_edges.append(segments.at[this_id, "edge"])
+            if this_wayid != segments.at[this_id, "wayid"]:
+                this_wayid = segments.at[this_id, "wayid"]
+                wayids.append(this_wayid)
+            end_node = this_node
+            length += segments.at[this_id, "length"]
 
-count = 0
-for start_id in segments.query('colour == segment').index:
-    if count % 8192 == 0:
-        time_offset('Output\t\t{:> 8} segments'.format(count))
+        output_segments.at[start_id, "target"] = end_node
+        output_segments.at[start_id, "length"] = length
+        output_segments.at[start_id, "wayids"] = wayids
+        output_segments.at[start_id, "nodes"] = these_nodes
+        count += 1
 
-    start_node = segments.at[start_id, 'source']
-    end_node = segments.at[start_id, 'target']
-    these_nodes = [start_node, end_node]
-    length = segments.at[start_id, 'length']
-    these_edges = [segments.at[start_id, 'edge']]
-    this_wayid = segments.at[start_id, 'wayid']
-    wayids = [this_wayid]
-    this_id = start_id
-    this_segment = segments.at[start_id, 'segment']
-    while this_id in links.index:
-        this_id = links.at[this_id, 'target_ix']
-        if links.at[start_id, 'loop'] and start_id == links.at[this_id, 'target_ix']:
-            break
-        this_node = segments.at[this_id, 'target']
-        if this_node == start_node:
-            break
-        these_nodes.append(this_node)
-        these_edges.append(segments.at[this_id, 'edge'])
-        if this_wayid != segments.at[this_id, 'wayid']:
-            this_wayid = segments.at[this_id, 'wayid']
-            wayids.append(this_wayid)
-        end_node = this_node
-        length += segments.at[this_id, 'length']
+    log(f"Output\t\t{len(output_segments):> 8} segments", args.quiet)
+    log("Writing segments", args.quiet, False)
 
-    output_segments.at[start_id, 'target'] = end_node
-    output_segments.at[start_id, 'length'] = length
-    output_segments.at[start_id, 'wayids'] = wayids
-    output_segments.at[start_id, 'nodes'] = these_nodes
-    count += 1
+    if args.outputfile:
+        output_segments.to_json(args.outputfile, orient="records", lines=True)
+    else:
+        output_segments.to_json(sys.stdout, orient="records", lines=True)
 
-time_offset('Output\t\t{:> 8} segments'.format(len(output_segments)))
-time_offset('Writing segments', False)
+    log(f"Written\t\t{len(output_segments):> 8} segments", args.quiet)
 
 
-if args.outputfile:
-    output_segments.to_json(args.outputfile, orient='records', lines=True)
-else:
-    output_segments.to_json(sys.stdout, orient='records', lines=True)
-
-time_offset('Written\t\t{:> 8} segments'.format(len(output_segments)))
+if __name__ == "__main__":
+    main()
