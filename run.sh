f2d879669695f8c67f586f0216fc8aae64d5bdb7
Will Deakin <will.deakin@crinstitute.org.uk>
2023-07-28 20:31:22 +0100

    add venv

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ ! -d venv ]; then
+    echo Set up python3 virtual environment
+    python3 -m venv venv
+    source venv/bin/activate
+    pip3 install --upgrade pip
+    pip3 install -r requirements.txt
+else
+    source venv/bin/activate
+fi
 
 ./junction.py --tsv testedges.tsv testsegments-file.jsonl
 
